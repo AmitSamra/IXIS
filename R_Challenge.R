7943eb19c23a541e40157753275c2cb5934771e0
Amit Samra <Amit.Samra@outlook.com>
2021-02-16 14:48:06 -0500

    Refactored Sheet 1 to group data by device

--- R_Challenge.R
+++ R_Challenge.R
@@ -1,4 +1,5 @@
 library(readr)
+library(tools)
 library(plyr)
 library(dplyr)
 library(ggplot2)
@@ -8,14 +9,12 @@ library(ggrepel)
 library(lubridate)
 library(openxlsx)
 
+
 # Load CSVs
 # --------------------------------------------------
 
 sessions_raw = read_csv("./data/DataAnalyst_Ecom_data_sessionCounts.csv")
-View(sessions_raw)
-
 cart_raw = read_csv("./data/DataAnalyst_Ecom_data_addsToCart.csv")
-View(cart_raw)
 
 
 # Clean Data
@@ -28,21 +27,32 @@ summary(cart_raw)
 # Change dim_date to date object
 sessions_raw$dim_date = as.Date(sessions_raw$dim_date, format = "%m/%d/%y")
 
+# Change column names
+names(sessions_raw) = c("Browser", "Device Category", "Date", "Sessions", "Transactions", "QTY")
+sessions_raw$"Device Category" = toTitleCase(sessions_raw$"Device Category")
+
 
 # Month*Device Aggregation
 # --------------------------------------------------
 
 # Subset sessions_raw
-df_MD = sessions_raw[c("dim_deviceCategory", "dim_date", "sessions", "transactions", "QTY")]
+df_MD = sessions_raw[c("Device Category", "Date", "Sessions", "Transactions", "QTY")]
+
+# Change date to month
+df_MD = df_MD %>% mutate(Date = floor_date(as_date(Date)))
 
 # Group by month
 df_MD = sessions_raw %>%
-  group_by(month=floor_date(dim_date, "year/month")) %>%
+  group_by(df_MD$"Device Category", Date) %>%
   summarize(
-    total_sessions = sum(sessions), 
-    total_transactions = sum(transactions), 
-    total_QTY = sum(QTY)
-    )
+    "Total Sessions" = sum(Sessions), 
+    "Total Transactions" = sum(Transactions), 
+    "Total QTY" = sum(QTY)
+  )
+
+# Order by date
+df_MD = df_MD[order(df_MD$"Date"), ]
+
 
 # Add ECR column
 df_MD$ECR = df_MD$total_transactions/df_MD$total_sessions
@@ -51,9 +61,10 @@ df_MD$ECR = format(round(df_MD$ECR,4), nsmall = 4)
 df_MD
 
 
+
 wb = createWorkbook("wb_Sessions_Car")
 addWorksheet(wb, "Sessions", gridLines = TRUE)
-setColWidths(wb, "Sessions", cols = 2:6, widths = 15)
+setColWidths(wb, "Sessions", cols = 1:6, widths = 15)
 writeData(wb, sheet = "Sessions", df_MD, colNames = TRUE, rowNames = FALSE)
 saveWorkbook(wb, "R_Challenge.xlsx", overwrite = TRUE)
 
